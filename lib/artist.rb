35ccb4ec1dbcbfd79e78befd62b70a589ac9e79b
Jennifer Stoos <jstoos@bitlaw.com>
2017-12-19 00:44:17 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -7,7 +7,7 @@ attr_accessor :name, :song, :songs
     @songs = []
   end
 
-  def add_song(song)
+  def self.add_song(song)
     # song = Song.new(song_name)
     @songs << song
     song.artist = self
