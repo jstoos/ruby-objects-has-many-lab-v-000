6569663933e6731cf2ddf739d640db95f735b074
Jennifer Stoos <jstoos@bitlaw.com>
2017-12-19 00:54:11 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,6 @@
 class Artist
 
-attr_accessor :name, :songs
+attr_accessor :name, :songs, :song
 
   def initialize(name)
     @name = name
@@ -13,17 +13,17 @@ attr_accessor :name, :songs
     song.artist = self
   end
 
-  def song=(song)
-    @song = song
-  end
-
-  def song
-    @song
-  end
-
-  def songs
-    @songs
-  end
+  # def song=(song)
+  #   @song = song
+  # end
+  # 
+  # def song
+  #   @song
+  # end
+  # 
+  # def songs
+  #   @songs
+  # end
 
   # expect(adele.songs).to include(hello)
   # expect(hello.artist).to eq(adele)
